9716d75a6f037877bfc194de5dc5c4f1edc7561d
ario07 <63526177+ario07@users.noreply.github.com>
2020-04-13 05:16:00 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4.ayoyo1 -t7
+./sugarmaker -a YespowerSugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q8qhmpxy3xg5v74f4vpydn74g7ey6gk9echq6c4.ayoyo1 -t3
